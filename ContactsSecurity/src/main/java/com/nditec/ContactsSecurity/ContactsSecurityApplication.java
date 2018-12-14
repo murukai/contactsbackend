829173c226a391766bb49b978f528fb328c82239
murukai <murukai.mberi@gmail.com>
2018-12-14 12:30:48 +0200

    Add files via upload

---
+++ ContactsSecurity/src/main/java/com/nditec/ContactsSecurity/ContactsSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.nditec.ContactsSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactsSecurityApplication.class, args);
+	}
+}
---
+++ ContactsSecurity/src/test/java/com/nditec/ContactsSecurity/ContactsSecurityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nditec.ContactsSecurity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContactsSecurityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
